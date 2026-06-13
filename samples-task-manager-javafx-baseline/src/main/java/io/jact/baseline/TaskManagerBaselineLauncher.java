6c37fe3fe943a4d2545e92567a4fbe77de09210f
Andrei Galitianu <andrei@galitianu.com>
2026-06-13 18:43:00 +0300

    fixed baseline

---
+++ samples-task-manager-javafx-baseline/src/main/java/io/jact/baseline/TaskManagerBaselineLauncher.java
@@ -0,0 +1,12 @@
+package io.jact.baseline;
+
+import javafx.application.Application;
+
+public final class TaskManagerBaselineLauncher {
+    private TaskManagerBaselineLauncher() {
+    }
+
+    public static void main(String[] args) {
+        Application.launch(TaskManagerBaselineApplication.class, args);
+    }
+}
